8e308dae8a1d14e3d1836ffee1d0b713e9060210
IkkiMachado <hique.reis@hotmail.com>
2025-06-26 17:59:43 -0300

    classe cliente e estrutura do projeto

---
+++ rentcar/apps/clientes/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField("Nome Completo", max_length=100)
+    cpf = models.CharField("CPF", max_length=11, unique=True)
+    cnh = models.CharField("CNH", max_length=20, unique=True)
+
+    class Meta:
+        verbose_name = "Cliente"
+        verbose_name_plural = "Clientes"
+        ordering =['id']
+    
+    def __str__(self):
+        return self.name
