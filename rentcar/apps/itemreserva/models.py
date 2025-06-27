611149637d2dd8c5560d43f527d397961a9b31fc
IkkiMachado <hique.reis@hotmail.com>
2025-06-26 21:39:47 -0300

    ajustes

--- rentcar/apps/itemreserva/models.py
+++ rentcar/apps/itemreserva/models.py
@@ -8,45 +8,87 @@ class ItemReserva(models.Model):
     reserva = models.ForeignKey(
         Reserva, 
         on_delete=models.CASCADE,
-        related_name='itens'
+        related_name='itens_reserva',
+        verbose_name='Reserva associada'
     )
     veiculo = models.ForeignKey(
         Veiculo,
         on_delete=models.PROTECT,
-        related_name='itemreserva'
+        related_name='itens_reserva',
+        verbose_name='Veículo reservado'
     )
-    data_inicio = models.DateField()
-    data_fim = models.DateField()
+    data_inicio = models.DateField(
+        verbose_name='Data de início',
+        help_text='Data de retirada do veículo'
+    )
+    data_fim = models.DateField(
+        verbose_name='Data de término',
+        help_text='Data prevista de devolução'
+    )
     valor_diaria = models.DecimalField(
         max_digits=10,
         decimal_places=2,
-        validators=[MinValueValidator(0)]
+        validators=[MinValueValidator(0)],
+        verbose_name='Valor da diária',
+        help_text='Valor por dia do veículo'
     )
     valor_parcial = models.DecimalField(
         max_digits=10,
         decimal_places=2,
-        validators=[MinValueValidator(0)]
+        validators=[MinValueValidator(0)],
+        verbose_name='Valor parcial',
+        help_text='Calculado automaticamente (diárias + acessórios)',
+        default=0
+    )
+    observacoes = models.TextField(
+        verbose_name='Observações',
+        blank=True,
+        null=True,
+        help_text='Informações adicionais sobre esta reserva'
     )
 
     def __str__(self):
-        return f"Item #{self.id} - {self.veiculo} ({self.data_inicio} a {self.data_fim})"
+        return f"Reserva #{self.reserva.id} - {self.veiculo.placa} ({self.data_inicio} a {self.data_fim})"
 
-    def calcular_valor_parcial(self):
-        """Calcula o valor parcial com base nos dias e acessórios"""
-        dias = (self.data_fim - self.data_inicio).days + 1
-        valor_acessorios = sum(
-            item.acessorio.preco_adicional * dias * item.quantidade
-            for item in self.itens_acessorios.all()
-        )
-        return (self.valor_diaria * dias) + valor_acessorios
+    def calcular_dias(self):
+        """Calcula a quantidade de dias da reserva"""
+        return (self.data_fim - self.data_inicio).days + 1
 
     def save(self, *args, **kwargs):
-        """Atualiza automaticamente o valor_parcial ao salvar"""
-        if not self.valor_parcial or self.pk is None:
-            self.valor_parcial = self.calcular_valor_parcial()
+        """
+        Salva o item primeiro para obter um ID,
+        depois calcula e atualiza o valor parcial
+        """
         super().save(*args, **kwargs)
-
+        
+        self.valor_parcial = self.calcular_valor_parcial()
+        
+        super().save(update_fields=['valor_parcial'])
+    
+    def calcular_valor_parcial(self):
+        """Versão segura que verifica se existe PK"""
+        if not self.pk:
+            return 0 
+            
+        valor_diarias = self.valor_diaria * self.calcular_dias()
+        valor_acessorios = self.calcular_valor_acessorios()
+        return valor_diarias + valor_acessorios
+    
+    def calcular_valor_acessorios(self):
+        """Versão segura que verifica se existe PK"""
+        if not self.pk:
+            return 0
+            
+        return sum(
+            acessorio.valor_total
+            for acessorio in self.veiculo_acessorios.all()
+        )
+    
     class Meta:
-        verbose_name = "itemreserva"
-        verbose_name_plural = "itemreserva"
+        verbose_name = 'Item de Reserva'
+        verbose_name_plural = 'Itens de Reserva'
         ordering = ['data_inicio']
+        indexes = [
+            models.Index(fields=['data_inicio', 'data_fim']),
+            models.Index(fields=['valor_parcial']),
+        ]
